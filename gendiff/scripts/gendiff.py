5196aec0eb60d1f49013644cde5b2997c37030b8
Azmont <alien1386@mail.ru>
2022-01-11 18:44:27 +0300

    2

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -8,7 +8,7 @@ def main():
     parser = argparse.ArgumentParser(description='Generate diff')
     parser.add_argument('file', metavar='first_file')
     parser.add_argument('file', metavar='second_file')
-
+    parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
     print(args.accumulate(args.integers))
 
